db768ada4bd3c4f4f9d524f9466f88faabdba011
Roomdada <dsieroger@gmail.com>
2025-09-25 21:40:44 +0000

    wip

--- app/Filament/Widgets/StateOverviewWidget.php
+++ app/Filament/Widgets/StateOverviewWidget.php
@@ -56,3 +56,4 @@ class StateOverviewWidget extends BaseWidget
         ];
     }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,5 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\PostController;
 
 Route::redirect('/', 'admin');
-
-Route::prefix('api')->group(function () {
-    Route::get('posts', [PostController::class, 'index']);
-    Route::get('posts/{slug}', [PostController::class, 'show']);
-});
